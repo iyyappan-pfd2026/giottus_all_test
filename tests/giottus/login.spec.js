7357164d014b77dc0be28093565ba975055646c5
iyyappan-pfd2026 <iyyappan.g.2021@gmail.com>
2026-06-09 16:09:00 +0530

    login test

---
+++ tests/giottus/login.spec.js
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test'
+import { permission } from 'node:process';
+
+test.use({
+  permissions: ['geolocation'],
+  geolocation: {
+    latitude: 13.0827,
+    longitude: 80.2707
+  }
+});
+
+test('login test', async ({page}) => {
+
+   
+    await page.goto('https://stage3.giottus.com/')
+    // await page.pause();
+    await page.getByRole('link', { name: 'login login' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('Selva@giottus.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Selva@123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('div').filter({ hasText: /^Allow Giottus to access your location$/ }).click();
+    await page.getByRole('textbox', { name: 'Enter code here' }).fill('123456');
+    // await page.close();    
+    
+})
