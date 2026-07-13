53df1e40bf98e3c267c3931210f901a5d5e641fb
iyyappan-pfd2026 <iyyappan.g.2021@gmail.com>
2026-07-13 18:31:27 +0530

    spot btc/inr limit order

--- tests/giottus/spot-trade/btc_inr.spec.js
+++ tests/giottus/spot-trade/btc_inr.spec.js
@@ -31,15 +31,66 @@ test.beforeEach(async ({ page }) => {
 
 })
 
-    test('Successful spot trade', async ({ page }) => {
 
-        
-        await page.getByRole('link', { name: 'trade trade Sub menu' }).click();
-        await page.getByText('Buy BTC').click();
-        await page.locator('div').filter({ hasText: /^BTC\/INR$/ }).click();        
-        await page.getByText('USDT', { exact: true }).click();
-        await page.getByRole('link', { name: 'BTC BTC/USDT' }).click();
-        await expect(page).toHaveURL(/tradeview\/BTC-USDT/);
+test.describe('Spot Trade BTC/INR Limit Order', () => { 
+
+            test('Successful spot trade Buy BTC/INR', async ({ page }) => {
+
+                
+                await page.getByRole('link', { name: 'trade trade Sub menu' }).click();
+                await page.getByText('Buy BTC').click();        
+                await page.getByRole('textbox').nth(1).click();
+                await page.getByRole('textbox').nth(1).fill('300'); // Enter the amount of INR you want to spend
+                const inrAmount = await page.getByRole('textbox').nth(1).inputValue();
+                const inramount = inrAmount?.replace(/,/g, '').toString(); // Format the INR amount to 2 decimal places
+                await page.getByRole('textbox').first().click();
+                await page.getByRole('textbox').first().fill('20'); // Enter the amount of BTC you want to buy
+                const btcAmount = await page.getByRole('textbox').first().inputValue();  
+                const btcamount = btcAmount?.replace(/,/g, '').toString(); // Format the BTC amount to 8 decimal places      
+                await page.getByRole('textbox').nth(2).click();
+                await page.getByRole('button', { name: 'Buy Now Right Arrow' }).click();        
+                await expect(page.getByText('Entered price is greater than')).toBeVisible();
+                await page.getByRole('button', { name: 'ContinueRight Arrow' }).click();
+                await page.getByRole('button', { name: 'Confirm Buy' }).click();
+                await expect(page.getByText(`Your Buy order of ${btcAmount}`)).toBeVisible();
+                await expect(page.locator('#table_26').getByText(inramount)).toBeVisible(); 
+                await expect(page.locator('#table_2').getByText(inramount)).toBeVisible(); 
 
 
-    });
+            });
+
+
+            test('spot trade Buy BTC/INR Empty Fields', async ({ page }) => {
+
+                
+                await page.getByRole('link', { name: 'trade trade Sub menu' }).click();
+                await page.getByText('Buy BTC').click();         
+                await page.getByRole('textbox').nth(1).click();
+                await page.getByRole('textbox').nth(1).fill('');
+                await page.getByRole('textbox').nth(2).click();
+                await page.getByRole('textbox').nth(2).fill('');
+                await page.getByRole('button', { name: 'Buy Now Right Arrow' }).click();
+                await expect(page.getByText('Invalid quantity entered!')).toBeVisible();
+                await page.getByRole('img', { name: 'close', exact: true }).click();
+
+            });
+
+            test('spot trade Buy BTC/INR less than minimum quantity', async ({ page }) => {
+
+                
+                await page.getByRole('link', { name: 'trade trade Sub menu' }).click();
+                await page.pause();
+                await page.getByText('Buy BTC').click();         
+                await page.getByRole('textbox').nth(1).click();
+                await page.getByRole('textbox').nth(1).fill('100');
+                await page.getByRole('textbox').first().click();
+                await page.getByRole('textbox').first().fill('0.00000099'); // minimum quantity is 0.00000100 BTC
+                await page.getByRole('textbox').nth(2).click();              
+                await page.getByRole('button', { name: 'Buy Now Right Arrow' }).click(); 
+                await expect(page.getByText('Minimum quantity should be 0.00000100')).toBeVisible();
+                await page.getByRole('img', { name: 'close', exact: true }).click();
+
+            });
+
+
+});
